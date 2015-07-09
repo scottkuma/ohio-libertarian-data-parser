4167923f153e2aa371980895dd49c79292c031b0
Scott Pettigrew <scott@scottkuma.net>
2015-07-09 10:32:45 -0400

    None this time.

--- scripts/6-TallyAllVoterAffiliations.py
+++ scripts/6-TallyAllVoterAffiliations.py
@@ -10,49 +10,47 @@ import shutil
 import time
 import zipfile
 import MyFunctions
-import pprint
 
 # Prepare to time execution - get start time
 start_time = time.time()
 
-# Set up variables needed later
-numVotersParsed = 0
-affiliations = []
-counties = {}
+
+
+
 
 zipFileDirectory = "../ZIP/"
+
 filePath = "../Database/"
 filePrefix = "Libertarians"
 fileSep = "-"
 fileSuffix = ".db"
 
-pp = pprint.PrettyPrinter(indent=4)
+
+#open CSV writer for output
+errorFile = csv.writer(open('errors.csv', 'wb'), delimiter=',')
+outFile = csv.writer(open('out.csv', 'wb'), delimiter=',')
 
 # each db output file is named with a time component to avoid over-writing files
 timeComponent = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 prelimDB = filePath+filePrefix+fileSuffix
-newDB = filePath+filePrefix+fileSep+timeComponent+fileSuffix
 
-# open CSV writer(s) for output
-outFileName = "OhioVoterAffiliations" + timeComponent + ".csv"
-outFile = open(outFileName, 'wb')
-outCSV = csv.writer(outFile, delimiter=',')
-
-errorFile = open('errors.csv', 'wb')
-errorCSV = csv.writer(errorFile, delimiter=',')
-
+#newDB = filePath+filePrefix+fileSep+timeComponent+fileSuffix
 
 # check if preliminary database exists, end if it doesn't
 # In the future, create the database if it isn't there.
-if os.path.isfile(prelimDB):
-    conn = sqlite3.connect(prelimDB)
-    cur = conn.cursor()
-else:
-    print "Preliminary Database File " + prelimDB + " Does Not Exist"
-    sys.exit()
+#if os.path.isfile(prelimDB):
+#    shutil.copyfile(prelimDB, newDB)
+#else:
+#    print "Preliminary Database File " + prelimDB + " Does Not Exist"
+#    sys.exit()
 
+# Connect to the database
+conn = sqlite3.connect(prelimDB)
+cur = conn.cursor()
 
 # get the list of fields from the database
+
 cur.execute("PRAGMA table_info('libertarians')")
 pragma = cur.fetchall()
 # extract db field names from
@@ -72,99 +70,42 @@ csvfiles.sort()
 
 # parse the datafiles
 for onefile in csvfiles:
-    countyAffiliations = {}
+    countyCount = 1
+    # Set up variables needed later
+    numVotersParsed = 0
+    numLibertarians = 0
 
     try:
-        # open the zip file for reading
         zf = zipfile.ZipFile(onefile, 'r')
     except zipfile.BadZipfile:
         print "\n\nBad Zip File."
         print "Should probably do something here..."
 
     except:
-        # Yes, catch-all exceptions are bad...
         print "\n\nERROR! " + filename + " ", sys.exc_info()[0]
     else:
+
         txtfilename = onefile[7:-4] + '.TXT'
-
-        # open the CSV file from within the ZIP file for reading
-        countyFile = zf.open(txtfilename)
-        reader = csv.reader(countyFile, delimiter=',')
-
-        # extract the county name from the filename string (remove the path & suffix)
+        print txtfilename
+        reader = csv.reader(zf.open(txtfilename), delimiter=',')
         countyname = onefile[7:-4]
         print countyname
-
         for row in reader:
-            # SANITY CHECK: Are there the correct # of fields in the record?
-            # If there aren't, then discard the line (writing it to an error file for later review)
             if len(row) == fieldsInLine:
-                # increment the # of voters parsed
                 numVotersParsed += 1
-                # find the voter affiliation
-                voterAffiliation = row[PARTY_AFFILIATION]
-                # "None" is more descriptive than *blank*
-                if voterAffiliation == '':
-                    voterAffiliation = "None"
-
-                # Does this voter affiliation exist in the county?
-                if voterAffiliation in countyAffiliations:
-                    # If so, tally another x-affiliated voter
-                    countyAffiliations[voterAffiliation] += 1
-                else:
-                    # If not, this is our first voter with this affiliation!
-                    # So add the affiliation to the county dictionary
-                    # Note: filtering out the string "PARTY_AFFILIATION" as this simply skips the header row
-                    if voterAffiliation != "PARTY_AFFILIATION":
-                        countyAffiliations[voterAffiliation] = 1
-
-                # Does this voter affiliation exist in the state?
-                # (We use the state affiliation list to ensure CSV consistency for counties without all affiliations.)
-                if voterAffiliation not in affiliations and voterAffiliation != "PARTY_AFFILIATION":
-                    affiliations.append(voterAffiliation)
-
+                if (row[PARTY_AFFILIATION] == 'L') or (row[LAST_PRIMARY_FIELD] == 'L'):
+                    numLibertarians += 1
+                    # print numVotersParsed, numLibertarians, "::", countyname, countyCount, "::", row[LAST_NAME], row[FIRST_NAME], (row[PARTY_AFFILIATION] if row[PARTY_AFFILIATION] != "" else "-"),(row[LAST_PRIMARY_FIELD] if row[LAST_PRIMARY_FIELD] != "" else "-")
+                    questionmarks = "?, " * (len(row) - 1)
+                    questionmarks += "?"
+                    # cur.execute("INSERT INTO Libertarians VALUES (" + questionmarks + ");", row)
+                    # conn.commit()
             else:
-                # Incorrect # of fields in the record, write the record out to the error file.
-                errorCSV.writerow(row)
-
-        # Close the county file!
-        countyFile.close()
-
-    # Store the county information in a dictionary.
-    counties[countyname] = countyAffiliations
-
-# Sort the affiliations so the output makes some sense.
-affiliations.sort()
-
-# Prepare output file header row
-h_row = ['COUNTY_NAME']
-for a in affiliations:
-    h_row.append(a)
-
-# Write the header row to the output file.
-outCSV.writerow(h_row)
-
-# Cycle through counties to output into header row.
-# NOTE: dictionaries are inherently un-ordered, so this will not output in alphabetical order
-# (This is easily-enough sorted in your database/spreadsheet program of choice.)
-for countyName, countyInfo in counties.iteritems():
-    # Prepare an empty  list for the row
-    c_row = []
-    c_row.append(countyName)
-    for a in affiliations:
-        if a in countyInfo:
-            c_row.append(countyInfo[a])
-        else:
-            c_row.append(0)
-    #Write the row to the CSV file
-    outCSV.writerow(c_row)
-
-
-#close files opened for writing
-errorFile.close()
-outFile.close()
-
-
+                #print "----> LINE SKIPPED: " + str(len(row))+ " fields <----"
+                errorFile.writerow(row)
+            countyCount += 1
+        outFile.writerow([onefile[7:-4],numLibertarians,numVotersParsed])
+        #conn.commit()
 
 end_time = time.time()
 total_time = end_time - start_time
@@ -172,4 +113,4 @@ votersPerSecond = numVotersParsed / total_time
 print("--- %s seconds ---" % str(total_time))
 print("--- %s voters parsed ---" % str(numVotersParsed))
 print("--- %s voters per second ---" % str(votersPerSecond))
-# print("--- %s Libertarians ---" % str(numLibertarians))
+print("--- %s Libertarians ---" % str(numLibertarians))
